7be724e69f32b227b27dc22b5403b2261f3c56ab
Drishti <drashti0602@gmail.com>
2026-05-08 08:33:38 +0530

    Add keyword identifier example file

---
+++ chapter_01_Basics/02_Keyword_identifier.js
@@ -0,0 +1,47 @@
+// ---------- KEYWORDS ----------let,const,var
+
+let name = "Drishti";
+const course = "JavaScript";
+var age = 26;
+
+
+// ---------- IDENTIFIERS ----------variable
+
+// Valid identifiers
+let firstName = "Drishti";
+let userAge = 24;
+let _salary = 50000;
+let $city = "Delhi";
+
+
+// Rules of identifiers:
+// Identifier cannot start with a number
+// Identifier cannot use keywords
+// Spaces are not allowed
+
+
+// ---------Invalid identifiers 
+// let 1name = "ABC";
+// let let = 10;
+// let user name = "Drishti";
+
+
+// ---------- LITERALS ----------
+
+let company = "xceedance";//xceedance
+let exp = 4;
+
+//---------- cases ----------
+
+let firstNameValue = "Drishti";// camelCase
+let my_age = 2;// snake_case
+let LastNameValue = "Gupta";// PascalCase
+
+
+
+// Single-line comment
+
+/*
+   Multi-line comment
+*/
+console.log("JavaScript Day 3 on 8 may- Done");
