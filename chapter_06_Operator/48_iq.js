a3315da3b0d4ecdc43a4609f47175eb3708bbeb1
Drishti <drashti0602@gmail.com>
2026-05-24 20:37:43 +0530

    feat: import chapter 7 if/else files from remote repo

---
+++ chapter_06_Operator/48_iq.js
@@ -0,0 +1,20 @@
+let a = 34;
+let result = a++;
+console.log(result);
+console.log(a); 
+
+
+
+
+// let ad = 10;
+// console.log(++ad + ad);
+// console.log(ad);
+
+
+// let ac = 10;
+// console.log(ac++ + ++ac);
+// console.log(ac);
+
+let ab = 10;
+console.log(++ab + ++ab);
+console.log(ab);
